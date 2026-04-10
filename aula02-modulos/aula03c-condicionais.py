136922dd3b16116ea624a1d1390eb1b7572d17f9
NyixTAA <davi66119@gmail.com>
2026-04-10 20:32:29 -0300

    Aula 3

---
+++ aula02-modulos/aula03c-condicionais.py
@@ -0,0 +1,33 @@
+idade = 20
+
+maior_idade = idade >= 18
+print(maior_idade, type(maior_idade))
+
+print()
+
+verifica_email = True
+verifica_senha = True
+
+login = verifica_email and verifica_senha
+print(login)
+
+if login:
+    print("Entrar no sistema")
+
+else:
+    print("Ow.. seja mais inteligente! Loga ai...")
+
+    # NOTAS
+
+nota_final = 6
+
+if nota_final < 4:
+    print("Reprovado")
+
+elif nota_final < 6:
+    print("Recuperação")
+
+else:
+    print("Aprovado")
+
+print("FIM")
