9f12462fcfaee6bfae303dada4fee152a14b97cb
labsfiap <davi66119@gmail.com>
2026-05-08 21:06:28 -0300

    .

---
+++ aula04-arrays/aula04a-arrays.py
@@ -0,0 +1,19 @@
+
+
+tamanho = len(lista_frutas)
+print(tamanho)
+print()
+
+for i in range(tamanho):
+    print(lista_frutas[i])
+
+print()
+
+for fruta in lista_frutas
+    print(fruta)
+
+msg = "Oi fulano!"
+print(msg[0])
+
+for i in range(len(msg)):
+    print(msg[i])
